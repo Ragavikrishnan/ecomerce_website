d80c389d29626ebc3b49cbbc9c58c552041f8131
Ragavikrishnan <ragavikrishnan03@gmail.com>
2026-03-20 19:15:02 +0530

    Fix

---
+++ frontend/src/components/Search.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Search() {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+  const seachbar = () => {
+    navigate("/search?keyword=" + keyword);
+  };
+
+  return (
+    <div className="input-group">
+      <input
+        type="text"
+        id="search_field"
+        onChange={(e) => setKeyword(e.target.value)}
+        className="form-control"
+        placeholder="Enter Product Name ..."
+        onBlur={seachbar}
+      />
+      <div className="input-group-append">
+        <button id="search_btn" className="btn" onClick={seachbar}>
+          <i className="fa fa-search" aria-hidden="true"></i>
+        </button>
+      </div>
+    </div>
+  );
+}
